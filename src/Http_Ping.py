63c8501ce8a580697586747f4126c4baa933986d
Tobias Brumhard <tobias.brumhard@mail.schwarz>
2019-09-05 09:27:02 +0200

    first prototype of http ping

--- src/Http_Ping.py
+++ src/Http_Ping.py
@@ -1,17 +1,37 @@
 import requests
 import time
+from datetime import datetime
+import pytz
+import email.utils as eut
+#dev:
+from urllib3.exceptions import InsecureRequestWarning
+
+# Suppress only the single warning from urllib3 needed.
+
 
 class Http_Ping:
     def __init__(self, connection_string):
         self.connection_string = connection_string
 
     def start(self):
+        session = requests.session()
+
         # verify false should not be used in prod
         # instead use path to cert file (https://2.python-requests.org/en/master/user/advanced/#ssl-cert-verification)
-        session = requests.session()
         session.verify = False
+        requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
         while True:
+            begin = datetime.utcnow()
+            
             response = session.get(self.connection_string)
-            http_time = response.headers['date']
-            print(http_time)
-            time.sleep(1)
+
+            now = datetime.utcnow()
+            # date header should not be used as is doesn't include milliseconds und GMT instead of UTC
+            # send_time = datetime.strptime(response.headers['date'], '%a, %d %b %Y %X %Z')
+            # send_time = send_time.astimezone(pytz.utc)
+            
+            time_delta = (now - begin).total_seconds()
+            # print("\nbegin:{0}, end: {1}".format(begin, now))
+            print("time consumed: {}\tStatus: {}".format(time_delta, response.status_code))
+            time.sleep(0.5)
--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 import requests
-import datetime
 from Http_Ping import Http_Ping
 
 test = Http_Ping('https://accounts-trn.lidl.com/')
